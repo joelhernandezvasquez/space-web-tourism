d862e475cb1159ba066b8f55cfad8a172796cb43
joelhernandezvasquez <jhernandez98@outlook.es>
2022-02-22 17:00:48 -0500

    added open and close functionality to the menu

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -1,14 +1,23 @@
-import React from 'react';
+
+import useToggle from './hooks/useToggle';
 import Logo from '../assets/shared/logo.svg';
 import MenuIcon from '../assets/shared/icon-hamburger.svg';
 import '../style/main.scss';
-//import CloseIcon from '../assets/shared/icon-close.svg';
+import CloseIcon from '../assets/shared/icon-close.svg';
 
 const MobileNavigation = () => {
+
+  const [toggle,handleToggle] = useToggle(false);
+
   return (
     <nav className='mobile-nav'>
       <img className='logo-icon' src={Logo} alt="logo"/>
-      <img className='menu-icon' src = {MenuIcon} alt="close-icon"/>
+      <img className='menu-icon' 
+      src = {!toggle? MenuIcon:CloseIcon} 
+      alt="close-icon"
+      onClick={handleToggle}
+      />
+
     </nav>
   )
 }
